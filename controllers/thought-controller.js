6cd139a848cce63a805c0d8f6bf04d156394391f
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-14 11:55:13 -0600

    fixed a bug where thoughts were not roving properly

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -22,6 +22,22 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+  // add reaction to a thought
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
   // get all thoughts
   getAllThought(req, res) {
     Thought.find({})
@@ -58,7 +74,23 @@ const thoughtController = {
   // delete thought
   deleteThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.thoughtId })
-      .then((dbThoughtData) => res.json(dbThoughtData))
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch((err) => res.json(err));
   },
 };
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -11,13 +11,12 @@ const {
 router.route("/").get(getAllThought);
 
 // /api/users/<thoughtId>
-router
-  .route("/:thoughtId")
-  .get(getThoughtById)
-  .put(updateThought)
-  .delete(deleteThought);
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
 
 // /api/thoughts/<userId>
 router.route("/:userId").post(addThought);
+
+// /api/thoughts/<userId>/<thoughtId>
+router.route("/:userId/:thoughtId").delete(deleteThought);
 
 module.exports = router;
