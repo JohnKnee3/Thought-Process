26cc605ed3f9953034eb07ce03a8e6a0d40bcba7
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-14 09:57:03 -0600

    Update a User working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -26,6 +26,21 @@ const userController = {
         res.sendStatus(400);
       });
   },
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,12 +3,13 @@ const {
   createUser,
   getAllUser,
   getUserById,
+  updateUser,
 } = require("../../controllers/user-controller");
 
 // /api/users
 router.route("/").post(createUser).get(getAllUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById);
+router.route("/:id").get(getUserById).put(updateUser);
 
 module.exports = router;
