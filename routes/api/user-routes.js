8668c6becc07c3dae032221a093c45fe2dfa3bb9
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-14 10:01:16 -0600

    delete a user working

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,12 +4,13 @@ const {
   getAllUser,
   getUserById,
   updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
 // /api/users
 router.route("/").post(createUser).get(getAllUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
